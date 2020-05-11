14cfb5f17c3ce57dcc7a5c4a41bce9383e247668
Richard White <w@rwhite.no>
2020-05-11 14:41:32 +0200

    First attempt at creating sc

---
+++ sc/R/2_onLoad.R
@@ -0,0 +1,82 @@
+.onLoad <- function(libname, pkgname) {
+  config$tasks <- TaskManager$new()
+
+  check_env_vars()
+
+  set_db()
+  set_computer_type()
+  set_progressr()
+
+  invisible()
+}
+
+check_env_vars <- function(){
+  needed <- c(
+    "SYKDOMSPULSEN_DB_DRIVER",
+    "SYKDOMSPULSEN_DB_SERVER",
+    "SYKDOMSPULSEN_DB_PORT",
+    "SYKDOMSPULSEN_DB_USER",
+    "SYKDOMSPULSEN_DB_PASSWORD",
+    "SYKDOMSPULSEN_DB_DB",
+    "SYKDOMSPULSEN_DB_TRUSTED_CONNECTION",
+
+    "SYKDOMSPULSEN_PRODUCTION",
+
+    "SYKDOMSPULSEN_PATH_INPUT",
+    "SYKDOMSPULSEN_PATH_OUTPUT"
+  )
+
+  for(i in needed){
+    getval <- Sys.getenv(i)
+    if(getval==""){
+      packageStartupMessage(crayon::red(glue::glue("{i}=''")))
+    } else {
+      packageStartupMessage(crayon::blue(glue::glue("{i}='{getval}'")))
+    }
+  }
+
+}
+
+set_db <- function(){
+  config$db_config <- list(
+    driver = Sys.getenv("SYKDOMSPULSEN_DB_DRIVER", "MySQL"),
+    server = Sys.getenv("SYKDOMSPULSEN_DB_SERVER", "db"),
+    port = as.integer(Sys.getenv("SYKDOMSPULSEN_DB_PORT", 1433)),
+    user = Sys.getenv("SYKDOMSPULSEN_DB_USER", "root"),
+    password = Sys.getenv("SYKDOMSPULSEN_DB_PASSWORD", "example"),
+    db = Sys.getenv("SYKDOMSPULSEN_DB_DB", "sykdomspuls"),
+    trusted_connection = Sys.getenv("SYKDOMSPULSEN_DB_TRUSTED_CONNECTION")
+  )
+
+  # rundate ----
+  add_schema(
+    name = "rundate",
+    schema = Schema$new(
+      db_config = config$db_config,
+      db_table = "rundate",
+      db_field_types = c(
+        "task" = "TEXT",
+        "date" = "DATE",
+        "datetime" = "DATETIME"
+      ),
+      db_load_folder = tempdir(),
+      keys = c(
+        "task"
+      )
+    )
+  )
+}
+
+set_computer_type <- function() {
+  if (Sys.getenv("SYKDOMSPULSEN_PRODUCTION") == "1") {
+    config$is_production <- TRUE
+  }
+}
+
+set_progressr <- function(){
+  progressr::handlers(progressr::handler_progress(
+    format = "[:bar] :current/:total (:percent) in :elapsedfull, eta: :eta",
+    clear = FALSE
+  ))
+}
+
