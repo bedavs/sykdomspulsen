12def1ed5f437a12ea683bfd0ce680cdb87787c4
Richard White <w@rwhite.no>
2020-05-19 06:14:38 +0200

    small fix in folder search for ui_normomo_ssi

--- sykdomspulsen/R/ui_normomo_ssi.R
+++ sykdomspulsen/R/ui_normomo_ssi.R
@@ -5,12 +5,14 @@
 #' @export
 ui_normomo_ssi <- function(data, argset, schema) {
   # tm_run_task("ui_normomo_ssi")
-  # data <- tm_get_data("ui_normomo_ssi", index_plan=1)
-  # argset <- tm_get_argset("ui_normomo_ssi", index_plan=1, index_argset = 1)
-  # schema <- tm_get_schema("ui_normomo_ssi")
 
+  if(plnr::is_run_directly()){
+    data <- sc::tm_get_data("ui_normomo_ssi", index_plan=1)
+    argset <- sc::tm_get_argset("ui_normomo_ssi", index_plan=1, index_argset = 1)
+    schema <- sc::tm_get_schema("ui_normomo_ssi")
+  }
 
-  folder <- fs::dir_ls(sc::path("output", "sykdomspulsen_normomo_restricted_output", create_dir = T))
+  folder <- fs::dir_ls(sc::path("output", "sykdomspulsen_normomo_restricted_output", create_dir = T), regexp = "[0-9][0-9][0-9][0-9]-[0-9][0-9]-[0-9][0-9]")
   folder <- max(folder)
   folder <- fs::dir_ls(fs::path(folder, "ssi"), regexp="norway")
   yrwk <- stringr::str_extract(folder, "[0-9][0-9][0-9][0-9]-[0-9][0-9]$")
